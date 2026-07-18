97f78be7d6374249e5e6fcb869959ce76d4cb64c
jeongmin <aceseojm@users.noreply.github.com>
2026-07-19 08:49:36 +0900

    Add social preview metadata

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 window.INSURANCE_CONFIG = {
   kakaoChatUrl: "",
-  siteBaseUrl: "",
+  siteBaseUrl: "https://kimwonja.com",
   sourcePageLabel: "insurance-landing",
   googleSheetWebhookUrl:
     "https://script.google.com/macros/s/AKfycbxdYUqXK73SHwbWytsqDGWE5V7yeFaEdghtDK5hGcvMJDK2NVB36JM5Kpa7TIHAH3pm/exec"
